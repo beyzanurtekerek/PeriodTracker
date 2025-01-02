31a8d244c503c2a19dd2f79a26b51bcc75045bea
Beyza Nur Tekerek <80324416+beyzanurtekerek@users.noreply.github.com>
2025-01-02 23:12:01 +0300

    Initial Commit

---
+++ PeriodTracker/PeriodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeriodTrackerApp.swift
+//  PeriodTracker
+//
+//  Created by Beyza Nur Tekerek on 2.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct PeriodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
